51ee8e4328b0d5c19c2c889b6516fd555334c473
lofo <louis.fo@gmail.com>
2025-03-13 08:56:36 +0100

    Initial Commit

---
+++ BeReal-TechnicalTest/BeReal_TechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeReal_TechnicalTestApp.swift
+//  BeReal-TechnicalTest
+//
+//  Created by Louis Fournier on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BeReal_TechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
